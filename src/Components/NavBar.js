135b5498bf17df4bde12a40d4bd69831d0e5c5f2
MullaN <admullan@gmail.com>
2021-02-22 10:44:09 -0800

    minor styling changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -10,15 +10,22 @@ const link = {
   fontSize: '1vw',
   alightItems: 'center'
 }
+
+const active = { 
+  background: 'white', 
+  color: 'black', 
+  borderBottomLeftRadius: '5px', 
+  borderBottomRightRadius: '5px' 
+}
  
 class Navbar extends React.Component {
   render() {
     return (
       <div className={"navBar"}>
-        <NavLink to="/" exact style={link} activeStyle={{ background: 'white', color: 'black' }}>Home</NavLink>
-        <NavLink to="/about" exact style={link} activeStyle={{ background: 'white', color: 'black' }}>About</NavLink>
-        <NavLink to="/blogs" exact style={link} activeStyle={{ background: 'white', color: 'black' }}>Blogs</NavLink>
-        <NavLink to="/projects" exact style={link} activeStyle={{ background: 'white', color: 'black' }}>Projects</NavLink>
+        <NavLink to="/" exact style={link} activeStyle={active}>Home</NavLink>
+        <NavLink to="/about" exact style={link} activeStyle={active}>About</NavLink>
+        <NavLink to="/blogs" exact style={link} activeStyle={active}>Blogs</NavLink>
+        <NavLink to="/projects" exact style={link} activeStyle={active}>Projects</NavLink>
       </div>
     )
   }
