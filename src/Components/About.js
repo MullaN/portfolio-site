7c4f663604f1fe23259400781e31a907ed50f2e4
MullaN <admullan@gmail.com>
2021-03-23 14:30:58 -0700

    fleshed out the about me section more

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,6 +13,11 @@ const About = () => {
             <p>In August 2020 I started the Software Engineering bootcamp at the Flatiorn School. The languages and frameworks covered were Ruby, Ruby on Rails, Javascript
                 and React, but I also learned how to think like a programmer and not be dependent on one language. I had a lot of fun throughout the course, meeting amazing people
                 and getting to work on fun and interesting projects. I graduated in the beginning of December 2020 and have now began my job search.
+            </p><br />
+            <p>
+                In my free time you will usually find me either watching sports or playing video games with my friends. I am a diehard fan of the Washington Huskies and Seattle Seahawks,
+                but I also follow and watch all of the big 4 leagues and college sports. The games I play are mostly multiplayer games with the occasional single player game if I have the time
+                and the story keeps me interested. When World of Warcraft is good and I haven't ran out of content to do, it dominates most of my gaming time. You can find my characters on my <a href="https://raider.io/user/MarioSanchez">raider.io profile page</a>.
             </p>
         </div>
     )
