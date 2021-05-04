eefe322de2e70a1fdf6ba2d6b2a58e5b071ad9c9
MullaN <admullan@gmail.com>
2021-05-03 17:46:59 -0700

    added banner scroll animation

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Banner = () => {
+    
+    const [scrolled, setScrolled] = useState(0)
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+
+        return function clear() {window.removeEventListener('scroll', handleScroll)}
+    })
+
+    const handleScroll = (e) => {
+        setScrolled(window.scrollY)
+    }
+
     return (
-        <div className="banner">
-            <img src="./IMG_0071.JPG" alt="Andrew Mullan"></img>
+        <div className="banner" style={{height: (window.innerHeight - scrolled)}}>
+            {scrolled ? <></> : <img src="./IMG_0071.JPG" alt="Andrew Mullan"></img>}
             <h1>Andrew Mullan</h1>
-            <h2>Javascript, React, Ruby, Ruby on Rails Developer</h2>
+            {scrolled ? <></> : <h2>Javascript, React, Ruby, Ruby on Rails Developer</h2>}
         </div>
     )
 }
